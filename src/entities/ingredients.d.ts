3eea5c404d11ad372bb26b9f66988e22b4da41f3
Wix Remote Machine <support@wix.com>
2025-12-02 09:32:04 +0000

    Manual code changes

--- src/entities/ingredients.d.ts
+++ src/entities/ingredients.d.ts
@@ -1,2 +1,4 @@
 // Re-export from main entities file for backward compatibility
 export type { Ingredients } from './index';
+// Re-export from main entities file for backward compatibility
+export type { IngredientsShowcase } from './index';
